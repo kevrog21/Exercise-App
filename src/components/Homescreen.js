90eb4136d70da3fd72a8d4a0cbfafb288c3560e1
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-03-03 00:08:44 -0700

    added day count placeholder to homescreen

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,7 +1,15 @@
+import starShape from '../assets/yellow_burst_shape.svg'
+
 function Homescreen() {
     return (
         <main>
-            homescreen is here
+            <div className='day-container'>
+                <div className='star-text-container'>
+                    <p className='star-text-day'>Day</p>
+                    <p className='star-text-day-number'>55</p>
+                </div>
+            </div>
+            
         </main>
     )
 }
