c0d598be1b1c9c520a5dad7d698a008e51f37df4
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-03-02 17:06:58 -0700

    added favicon and began building header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
+import logo from '../assets/dfc_logo.svg'
+
 
 function Header() {
     return (
-        <header className="header">header goes here</header>
+        <header className="header">
+            <div className='header-container'>
+                <img className='header-logo' src={logo}></img>
+                <h2 className='app-title'>Daily Fit Challenge</h2>
+                <div className='hamburger-container'>
+                    <div className='hamburger-line-1'></div>
+                    <div className='hamburger-line-2'></div>
+                    <div className='hamburger-line-3'></div>
+                </div>
+            </div>
+        </header>
     )
 }
 
-export default Header 
+export default Header
