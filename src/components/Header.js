72418960876d48cc83896534a15d9d2c3c788e41
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-03-12 19:47:02 -0700

    added functionality to side nav toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,67 @@
 import logo from '../assets/dfc_logo.svg'
 import SideNav from './SideNav'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
+
+    const [isSideNavOpen, setIsSideNavOpen] = useState(false)
+    const [navMenuInteractedWith, setNavMenuInteractedWith] = useState(false)
+
+    function showAndDisableNavMenu() {
+        const container = document.querySelector('.nav-menu-container')
+        const navMenu = document.querySelector('#nav-menu')
+        const mainEl = document.querySelector('main')
+
+        if (container) {
+            if (isSideNavOpen) {
+                container.classList.add('show')
+                navMenu.classList.add('show')
+                mainEl.classList.add('disable-pointer-events')
+                document.documentElement.style.overflow = 'hidden'
+                document.body.style.overflow = 'hidden'
+                
+            } else {
+                container.classList.remove('show')
+                navMenu.classList.remove('show')
+                mainEl.classList.remove('disable-pointer-events')
+                document.documentElement.style.overflow = ''
+                document.body.style.overflow = ''
+            }
+        }
+    }
+
+    useEffect(() => {
+        showAndDisableNavMenu()
+    }, [isSideNavOpen])
+
+    const handleMenuToggle = () => {
+        setIsSideNavOpen((prevIsNavMenuOpen) => !prevIsNavMenuOpen)
+        setNavMenuInteractedWith(true)
+    }
+
+    const handleNavItemClick = () => {
+        setIsSideNavOpen(false)
+    }
+
     return (
         <div>
             <header className="header">
                 <div className='header-container'>
                     <img className='header-logo' src={logo}></img>
                     <h2 className='app-title'>Daily Fit Challenge</h2>
-                    <div className='hamburger-container'>
+                    <div className='hamburger-container' onClick={handleMenuToggle}>
                         <div className='hamburger-line-1'></div>
                         <div className='hamburger-line-2'></div>
                         <div className='hamburger-line-3'></div>
                     </div>
                 </div>
             </header>
-            <SideNav />
+            <SideNav 
+                isSideNavOpen={isSideNavOpen}
+                setIsSideNavOpen={setIsSideNavOpen}
+                handleNavItemClick={handleNavItemClick}
+                showAndDisableNavMenu={showAndDisableNavMenu}
+            />
         </div>
     )
 }
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,10 +1,20 @@
+import { useEffect } from "react"
 
-export default function SideNav() {
+export default function SideNav(props) {
 
+    function closeNavMenu() {
+        props.setIsSideNavOpen(false)
+    }
+    
+    useEffect(() => {
+        props.showAndDisableNavMenu()
+    }, [props.isNavMenuOpen])
 
     return (
-        <div>
-            
+        <div className='nav-menu-container' onClick={closeNavMenu} style={props.isNavMenuOpen ? {} : {pointerEvents: 'none'}}>
+             <div id='nav-menu' className=''>
+
+             </div>
         </div>
     )
 }
