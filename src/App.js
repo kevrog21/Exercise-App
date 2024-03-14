193e86f01b5a74bda2956dfe2682d4eab93085a7
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-03-14 14:05:46 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Homescreen from './components/Homescreen'
 import Footer from './components/Footer'
@@ -7,9 +8,16 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Homescreen />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={
+            <Homescreen 
+              
+            />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
