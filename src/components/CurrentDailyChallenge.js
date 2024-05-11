16953b99a67fd2b64bd54d2148a823199ff5e309
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-05-10 22:14:10 -0700

    began currentDailyChallenge form

--- src/components/CurrentDailyChallenge.js
+++ src/components/CurrentDailyChallenge.js
@@ -16,6 +16,15 @@ export default function CurrentDailyChallenge(props) {
 
     useEffect(() => {
         if (currentUserWorkoutData) {
+            const initialFormData = currentUserWorkoutData.dailyRoutine.reduce((acc, exercise, index) => {
+                return { ...acc, [exercise.exerciseName]: 0}
+            }, {honeyp: '', pword: ''})
+            setFormData(initialFormData)
+        }
+    }, [currentUserWorkoutData])
+
+    useEffect(() => {
+        if (currentUserWorkoutData) {
             const dailyChallengeExercises = currentUserWorkoutData.dailyRoutine.map((exercise, index) => {
                 return (
                     <div key={index} className='current-workout-list-item'>
@@ -26,6 +35,13 @@ export default function CurrentDailyChallenge(props) {
                                 </div>
                                 <div className='rep-scroller-container'>
                                     <div className='current-rep-label'>current</div>
+                                    <div className='rep-scroller'>
+                                        <div className='current-rep'>0</div>
+                                        <div className='temp-button-container'>
+                                            <div className='temp-button'>-</div>
+                                            <div className='temp-button'>+</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             {exercise.unit === 'seconds' && 
@@ -53,12 +69,43 @@ export default function CurrentDailyChallenge(props) {
         }
     }, [currentUserWorkoutData])
 
+    const handleFormChange = (event) => {
+        const { name, value } = event.target
+        const newValue = parseInt(value)
+
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: newValue
+        }))
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const incompleteMessageEl = document.getElementById('incomplete-workout-message')
+        const successMessageEl = document.getElementById('success-message')
+        const incorrectPasswordEl = document.getElementById('incorrect-password-message')
+        const alreadyCompletedWorkoutEl = document.getElementById('already-completed-message')
+
+        console.log(formData)
+    }
+
     return (
         <main className="page-margin-top">
-            <div className="day-title">
-                Day <span className="day-title-number">{allExerciseEls.length > 0 && currentUserWorkoutData.workouts.length + 1}</span>
-            </div>
-            {allExerciseEls}
+            {showTimer && 
+                <Timer 
+                    showTimer={showTimer}
+                    setShowTimer={setShowTimer}
+                    timerTime={timerTime}
+                />}
+            <form onSubmit={handleSubmit}>
+                <div className="day-title">
+                    Day <span className="day-title-number">{allExerciseEls.length > 0 && currentUserWorkoutData.workouts.length + 1}</span>
+                </div>
+                {allExerciseEls}
+                <input type='text' name='honeyp' className='honeyp' value={formData.honeyp} onChange={handleFormChange} tabIndex='-1' autoComplete="off"></input>
+                <input type='password' name='pword' className="password-input" value={formData.pword} onChange={handleFormChange}></input>
+                <button type="submit" className="submit-btn" id="submit" >Submit Workout</button>
+            </form>
 
 
         </main>
