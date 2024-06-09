cdaa66ce854f6b808800792a66c94347f0be8297
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-06-09 15:49:26 -0700

    added theme selector to settings page

--- src/components/UserSetings.js
+++ src/components/UserSetings.js
@@ -2,14 +2,22 @@
 export default function Settings() {
     return (
         <main>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
-            <div>Settings</div>
+            <div className='settings-contianer page-margin-top'>
+                <div className='settings-header'>Settings</div>
+                <form>
+                    <label htmlFor="theme">Theme</label>
+                    <select 
+                        className='theme-input'
+                        name="theme"
+                        value={''}
+                        onChange={console.log('changed')}
+                    >
+                        <option value="light-mode">light mode</option>
+                        <option value="dark-mode">dark mode</option>
+                        <option value="beast-mode">beast mode</option>
+                    </select>
+                </form>
+            </div>
         </main>
     )
 }
