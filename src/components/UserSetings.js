c9235247609aeb3705914f9567d93c575020b20e
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-06-10 14:15:44 -0700

    added basic form structure / functionality to settings component

--- src/components/UserSetings.js
+++ src/components/UserSetings.js
@@ -1,5 +1,18 @@
+import { useState } from 'react'
 
 export default function Settings() {
+
+    const [settingsFormData, setSettingsFormData] = useState({})
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+
+        setSettingsFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className='settings-contianer page-margin-top'>
@@ -9,13 +22,16 @@ export default function Settings() {
                     <select 
                         className='theme-input'
                         name="theme"
-                        value={''}
-                        onChange={console.log('changed')}
+                        value={settingsFormData.theme}
+                        onChange={handleInputChange}
                     >
                         <option value="light-mode">light mode</option>
                         <option value="dark-mode">dark mode</option>
                         <option value="beast-mode">beast mode</option>
                     </select>
+                    <div>
+                        <button type='submit' className='settings-submit-btn'>update settings</button>
+                    </div>
                 </form>
             </div>
         </main>
