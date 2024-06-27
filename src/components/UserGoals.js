e6d9f540ddd25d459c99b8f389e4e2b1aac3112d
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-06-26 18:08:44 -0700

    updated goals component

--- src/components/UserGoals.js
+++ src/components/UserGoals.js
@@ -1,15 +1,28 @@
+import { useNavigateToLink } from './ToHomeScreen'
 
 export default function Goals() {
+
+    const navigate = useNavigateToLink()
+
+    const handleBackButtonClick = () => {
+        navigate('/')
+    }
+
     return (
         <main>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
-            <div>Goals</div>
+            <div className='page-margin-top'>
+                <div className='back-arrow-container' onClick={handleBackButtonClick}>
+                    <div className='back-arrow'></div>
+                    <div className='back-arrow-tail'></div>
+                </div>
+            
+                <div className='goals-container'>
+                    <div className='goals-header'>Goals</div>
+                    <p>- Reach level 300 by 3/20/25</p>
+                    <p>- Go to the gym 50 times by 3/20/25</p>
+                    <p>- Complete 25 sprint training sessions by 3/20/25</p>
+                </div>
+            </div>
         </main>
     )
 }
