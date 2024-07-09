14f74e3bd989a75ecfe23efa4517154f83fe0236
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-07-08 22:16:55 -0700

    added content outline to exercise index page

--- src/components/ExerciseIndex.js
+++ src/components/ExerciseIndex.js
@@ -4,8 +4,31 @@ export default function Rules() {
 
     return (
         <main className='page-margin-top'>
-            <div>
-                Exercise Index
+            <div className="exercise-index-section">Arm Exercises</div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
+            </div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
+            </div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
+            </div>
+            <div className="exercise-index-section">Back Exercises</div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
+            </div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
+            </div>
+            <div className='exercise-item-container'>
+                <div>Title</div>
+                <div>Description</div>
             </div>
         </main>
     )
