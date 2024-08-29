99785c5e7d842810656642abd5e0009fde243d8a
kevrog21 <112293086+kevrog21@users.noreply.github.com>
2024-08-28 20:42:17 -0700

    updated come back tomorrow text to only show for the first 10 levels

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -53,7 +53,7 @@ function Homescreen(props) {
                 {userCompletedTodaysWorkout && 
                 <div>
                     <div className='workout-completed-msg'>You completed your workout for the day!</div>
-                    <div className='come-back-msg'>come back tomorrow to complete level {mostRecentCompletedChallengeData.challengeNumber + 1}</div>
+                    {(Object.keys(mostRecentCompletedChallengeData).length > 0 ? mostRecentCompletedChallengeData.challengeNumber < 11 ? true : false : false) && <div className='come-back-msg'>come back tomorrow to complete level {mostRecentCompletedChallengeData.challengeNumber + 1}</div>}
                 </div>}
             </div>
             }
